bc52d1cd32980d24f21a39c749bd20600178fb34
Tianfeng Lu <tianfeng-lu@zju.edu.cn>
2024-01-16 09:15:19 +0800

    update r notebook

--- download_data.r
+++ download_data.r
@@ -1,17 +1,13 @@
 # fetch data from zenodo
-
-library(httr)
+library(curl)
 
 url <- "https://zenodo.org/api/records/10139562/files-archive"
 
 
-response <- GET(url, progress())
+curl_download(url, destfile = "./tmp.zip", quiet = FALSE)
 
-if (status_code(response) == 200) {
-  writeBin(content(response, "raw"), "./tmp.zip")
-  unzip(destination_zip, exdir = "./")
-  cat("File downloaded successfully.\n")
-} else {
-  cat("Failed to download the file. Status code:", status_code(response), "\n")
-}
+unzip("./tmp.zip", exdir = "./")
+cat("File downloaded successfully.\n")
+
+
 
